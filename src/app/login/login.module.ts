b3e29bec95726bb0aa22457cbb6c0781e1d282d7
Tahsin Ahmed <tahsin.ahmed23@brainstation-23.com>
2022-03-23 18:40:36 +0600

    made login page

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
 
-
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+  ],
   imports: [
-    CommonModule
+    ReactiveFormsModule,
+    CommonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
   ]
 })
 export class LoginModule { }
